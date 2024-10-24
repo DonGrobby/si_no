51530e4e30725e4517a31cf6a8be321cc77e3c17
DonGrobby <webpracticexampp@gmail.com>
2024-10-24 07:24:04 -0700

    Resetear marcador con código Konami

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -22,6 +22,14 @@ class SipiBloc extends Bloc<Eventos, SipiState> {
   int get puntuacionMaxima => _puntuacionMaxima;
   bool recordSuperado = false;
   late SharedPreferencesAsync shared;
+  int codeIndex = 0;
+  Map<int, TipoRespuesta> konamiCode = {
+    0: TipoRespuesta.no,
+    1: TipoRespuesta.no,
+    2: TipoRespuesta.si,
+    3: TipoRespuesta.si,
+    4: TipoRespuesta.no,
+  };
   SipiBloc() : super(Inicial(0)) {
     on<PuntuacionMaximaCargado>((event, emit) async {
       shared = SharedPreferencesAsync();
@@ -35,14 +43,13 @@ class SipiBloc extends Bloc<Eventos, SipiState> {
       late http.Response respuesta;
       try {
         respuesta = await http.get(uri);
+        if (respuesta.statusCode == 200) {
+          var modelo = Modelo.fromJson(jsonDecode(respuesta.body));
+          add(Respondio(modelo));
+          return;
+        }
       } catch (e) {
         emit(EstadoFallido());
-      }
-
-      if (respuesta.statusCode == 200) {
-        var modelo = Modelo.fromJson(jsonDecode(respuesta.body));
-        add(Respondio(modelo));
-        return;
       }
       emit(EstadoFallido());
     });
@@ -57,8 +64,12 @@ class SipiBloc extends Bloc<Eventos, SipiState> {
       emit(EstadoRespuesta(event.modelo));
     });
     on<Respondio>((event, emit) async {
+      print(codeIndex);
+      print("${konamiCode[codeIndex]} $ultimaRespuesta");
       var queRespondio = cadenaARespuesta(event.modelo.answer);
       if (queRespondio == ultimaRespuesta) _puntuacion++;
+      if (konamiCode[codeIndex] == ultimaRespuesta) codeIndex++;
+      if (codeIndex == konamiCode.length) add(PuntuacionMaximaBorrado());
       if (_puntuacion > _puntuacionMaxima) {
         add(RecordSuperado(event.modelo));
         return;
@@ -70,6 +81,7 @@ class SipiBloc extends Bloc<Eventos, SipiState> {
     on<PuntuacionMaximaBorrado>((event, emit) async {
       shared.setInt(sharedPuntuacionMaxima, 0);
       _puntuacionMaxima = 0;
+      codeIndex = 0;
       emit(Inicial(0));
     });
   }
